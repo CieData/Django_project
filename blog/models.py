a8233b01d26a8c668103ef9310e0450339657c12
github <ehdud1357@naver.com>
2023-01-12 17:52:38 +0900

    team

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 #	Create	your	models	here.
 class Post(models.Model):
-    title	=	models.CharField(max_length=30)	#	데이터베이스의 필드(컬럼)
-    content	=	models.TextField()
+    name	=	models.CharField(max_length=30)	#	데이터베이스의 필드(컬럼)
     created_at =	models.DateTimeField(auto_now_add=True)
     updated_at =	models.DateTimeField(auto_now=True)
+    introduce	=	models.TextField()
+
 #	author는 추후 작성
 def __str__(self):
-    return f'[{self.pk}]{self.title}'
+    return f'[{self.pk}] {self.title}'
